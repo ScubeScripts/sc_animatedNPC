bfc0d0ec4e6ef55aa4ca59eaa6d4a077b03842a5
Scube Scripts <104854776+ScubeScripts@users.noreply.github.com>
2023-07-25 20:17:44 +0200

    Update peds_config.lua

--- peds_config.lua
+++ peds_config.lua
@@ -1,3 +1,10 @@
+-- ░██████╗░█████╗░██╗░░░██╗██████╗░███████╗░██████╗░█████╗░██████╗░██╗██████╗░████████╗░██████╗
+-- ██╔════╝██╔══██╗██║░░░██║██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝██╔════╝
+-- ╚█████╗░██║░░╚═╝██║░░░██║██████╦╝█████╗░░╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░╚█████╗░
+-- ░╚═══██╗██║░░██╗██║░░░██║██╔══██╗██╔══╝░░░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░░╚═══██╗
+-- ██████╔╝╚█████╔╝╚██████╔╝██████╦╝███████╗██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░██████╔╝
+-- ╚═════╝░░╚════╝░░╚═════╝░╚═════╝░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═════╝░   
+
 Config = {}
 
 -- The distance at which NPCs will be spawned/despawned
@@ -5,46 +12,26 @@ Config.SpawnDistance = 400
 
 Config.Peds = {
 
---------------------------------------Galaxy Club--------------------------------------
-	{
-		model = 'u_f_y_dancerave_01',
-		animation = {
-			dict = 'anim@amb@nightclub@dancers@solomun_entourage@',
-			name = 'mi_dance_facedj_17_v1_female^1',
-		},
+--------------------------------------Examples--------------------------------------
+{
+ 	model = 'u_f_y_dancerave_01',  -- NPC Model -> https://wiki.rage.mp/index.php?title=Peds
+ 	animation = {
+ 		dict = 'anim@amb@nightclub@dancers@solomun_entourage@', -- NPC Animation Header
+ 		name = 'mi_dance_facedj_17_v1_female^1', -- NPC Animation -> https://alexguirre.github.io/animations-list/
+ 	},
+ 	locations = {
+ 		{ x = -1598.45,  y = -3015.54, z = -79.15, heading = 330.43 } -- NPC Location
+ 	}
+ },
 
-		locations = {
-
-			{ x = -1598.45,  y = -3015.54, z = -79.15, heading = 330.43 }
-
-		}
-	},
-	{
-		model = 'u_m_y_dancerave_01',
-		animation = {
-			dict = 'anim@amb@nightclub@dancers@solomun_entourage@',
-			name = 'mi_dance_facedj_17_v1_female^1',
-		},
-
-		locations = {
-
-			{ x = -1596.14,  y = -3008.02, z = -79.15, heading = 174.73 }
-
-		}
-	},
-
-	--------------------------------------Mission Row Police Departement--------------------------------------
-
-	{
-		model = 's_m_m_armoured_01',
-		weapons = {
-			{name = 'weapon_smg', minAmmo = 20, maxAmmo = 100}
-		},
-		defaultWeapon = 'weapon_smg',
-		locations = {
-
-			{ x = 480.43,  y = -996.67, z = 29.69, heading = 88.69 }
-
-		}
-	},
+{
+ 	model = 's_m_m_armoured_01', -- NPC Model -> 
+ 	weapons = {
+ 		{name = 'weapon_smg', minAmmo = 20, maxAmmo = 100}
+ 	},
+ 	defaultWeapon = 'weapon_smg', -- Weapon Model ->
+ 	locations = {
+ 		{ x = 480.43,  y = -996.67, z = 29.69, heading = 88.69 } -- NPC Location
+ 	}
+ },
 }
